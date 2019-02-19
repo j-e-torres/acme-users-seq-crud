001c2f2369196e83992dbee8f510d6e39fded98e
Juan Torres <jet@Juans-MacBook-Pro.local>
2019-02-18 21:35:03 -0500

    added update functionality, done

--- app.js
+++ app.js
@@ -9,13 +9,56 @@ app.use(express.urlencoded({ extended: false}));
 app.use((req, res, next) => {
     User.findAll()
         .then(users => {
-            // req.firstName = users.firstName
-            // req.lastName = users.lastName
             req.users = users;
             next()
         })
         .catch(next);
-    })
+})
+
+const renderUserUpdate = (users, user) => {
+    return `<!DOCTYPE html>
+    <html>
+        <head>
+            <title> Acme Users CRUD-SEQ </title>
+            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+
+        </head>
+
+        <body>
+            <div class='container'>
+                <h1> Acme Users CRUD-SEQ </h1>
+                <ul>
+                    ${users.map(user => {
+                        return `
+                            <li class='list-group-item'>
+                                <a href='/users/${user.id}'>
+                                ${user.firstName} ${user.lastName}
+                                </a>
+
+                                <form method='post' action='/users/${user.id}/?_method=delete'>
+                                    <button class='btn btn-danger'> Delete </button>
+                                </form>
+                            </li>
+                        `
+                    }).join('')}
+                </ul>
+
+                <div style='border: solid 1px black; border-radius: 5px; margin-top: 10px; padding: 10px'>
+                    <form method='post' action='/users/${user.id}/?_method=put'>
+
+                        <input name='firstName' value='${user.firstName}'>
+                        <input name='lastName' value='${user.lastName}'>
+                        
+                        <button> Update </button>
+
+                        <a href='/users'> Cancel </a>
+                    </form>
+                </div>
+            </div>
+        </body>
+    </html>
+    `
+}
 
 const renderUsers = (users) => {
     return `<!DOCTYPE html>
@@ -88,6 +131,22 @@ app.delete('/users/:id', (req, res, next) => {
         }
     })
     .then(() => res.redirect('/users'))
+    .catch(next);
+})
+
+app.get('/users/:id', (req, res, next) => {
+    User.findByPk(req.params.id)
+    .then((user) => res.send(renderUserUpdate(req.users, user)))
+    .catch(next);
+})
+
+app.put('/users/:id', (req, res, next) => {
+    User.update({firstName: req.body.firstName, lastName: req.body.lastName}, {
+        where: {
+        id: req.params.id
+    }
+    })
+    .then(() => res.redirect(`/users`))
     .catch(next);
 })
 
